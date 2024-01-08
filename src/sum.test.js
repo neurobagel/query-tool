bdf9818730587cb3aca95ecf6f795c0abc25869e
Sebastian Urchs <sebastian.urchs@gmail.com>
2024-01-08 16:17:42 -0500

    Setup vitest

---
+++ src/sum.test.js
@@ -0,0 +1,8 @@
+// sum.test.js
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { expect, test } from 'vitest'
+import { sum } from './sum'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
+})
