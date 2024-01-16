f4368d4a56a28bb4846835b8b406f7bac1bd8d8e
rmanaem <rmanaem@protonmail.ch>
2024-01-16 18:49:54 -0500

    Removed dead code

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  e2e: {},
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
