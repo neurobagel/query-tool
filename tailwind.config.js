6256fd4915a5c5e99426a1f898565a83aa5fd16a
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2024-01-18 09:59:14 -0500

    [ENH] Implemented logic for node selection (#10)
    * WIP layout using mui Grid
    
    * WIP Switched layout to tailwind grid classes
    
    * Updated the `ResultCard` layout
    
    * Updated `ResultContainer` component layout
    
    * Renamed and moved example files
    
    * Added constants module
    
    * Implemented query fields in the layout
    
    * Removed dead code
    
    * [WIP] Implemented logic for sending requests to the API
    
    * Implemented logic for sending requests to the API
    
    * Removed commented code
    
    * Bumped mui
    
    * Implemented logic for node selection
    
    * Modified tailwind grid template
    
    to allow row expansion independently
    
    * Fixed the option filtering issue of `Autocomplete`

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '7': 'repeat(7, auto)',
+        '8': 'repeat(8, auto)',
+      },
+    },
   },
   plugins: [],
   corePlugins: {
