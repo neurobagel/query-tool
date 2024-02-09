da4a98a4ada65cbbe8cca9846dc293873b6747d8
rmanaem <rmanaem@protonmail.ch>
2024-02-09 18:52:32 -0500

    Set the port for `preview` to 5173

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) =>{
   const envVars = loadEnv(mode, process.cwd(), '');
+  console.log(process.env);
   if (!envVars.NB_API_QUERY_URL) {
     throw new Error(`Environment variable NB_API_QUERY_URL is not defined.`);
   }
 
   return {
+    preview:{
+      port: 5173,
+    },
     plugins: [react()],
     envPrefix: 'NB_'
   }
