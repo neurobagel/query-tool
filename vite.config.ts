5426170ecc928db22afc97bbdc8074e6388e408e
rmanaem <rmanaem@protonmail.ch>
2024-02-09 14:30:23 -0500

    Implemented check for `NB_API_QUERY_URL` env var

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() =>{
+    if (!process.env.NB_API_QUERY_URL) {
+    throw new Error(`Environment variable NB_API_QUERY_URL is not defined.`);
+  }
+
+  return {
+    plugins: [react()],
+    envPrefix: 'NB_'
+  }
 });
