3a6f41afc3aa72d2a7c8f3d37aeaafac2a88f7af
rmanaem <rmanaem@protonmail.ch>
2024-02-09 15:44:14 -0500

    Fixed the issue with envvar check

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(() =>{
-    if (!process.env.NB_API_QUERY_URL) {
+  const envVars = loadEnv('', process.cwd(), '');
+  if (!envVars.NB_API_QUERY_URL) {
     throw new Error(`Environment variable NB_API_QUERY_URL is not defined.`);
   }
 
