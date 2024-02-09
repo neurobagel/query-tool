47f3f825ee914e38926b05115defb28e4953621b
rmanaem <rmanaem@protonmail.ch>
2024-02-09 15:51:22 -0500

    Refactored the env var check

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(() =>{
-  const envVars = loadEnv('', process.cwd(), '');
+export default defineConfig(({ mode }) =>{
+  const envVars = loadEnv(mode, process.cwd(), '');
   if (!envVars.NB_API_QUERY_URL) {
     throw new Error(`Environment variable NB_API_QUERY_URL is not defined.`);
   }
