a972da34c1a35888ea14a67c00f03d27699a679f
rmanaem <rmanaem@protonmail.ch>
2024-02-09 18:55:02 -0500

    Updated `e2e-test` workflow file
    Removed `console.log` statement

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) =>{
   const envVars = loadEnv(mode, process.cwd(), '');
-  console.log(process.env);
   if (!envVars.NB_API_QUERY_URL) {
     throw new Error(`Environment variable NB_API_QUERY_URL is not defined.`);
   }
