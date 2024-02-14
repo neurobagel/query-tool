8220bd318dc36fb24e6cbd0d39767e6f68638aec
rmanaem <rmanaem@protonmail.ch>
2024-02-14 15:07:28 -0500

    [REF] Added empty line between logical blocks

--- cypress/e2e/Alert.cy.ts
+++ cypress/e2e/Alert.cy.ts
@@ -9,13 +9,17 @@ describe('Alert', () => {
       // dropdown with nodes before searching for OpenNeuro
       cy.wait('@getNodes');
       cy.get('[data-cy="openneuro-alert"]').should('be.visible').should('contain', 'The OpenNeuro node is being actively annotated at the participant');
+
       cy.get('[data-cy="Neurobagel graph-categorical-field"]').type('Quebec Parkinson Network{downarrow}{enter}');
       cy.get('[data-cy="openneuro-alert"]').should('not.exist');
+      
       cy.get('[data-cy="Neurobagel graph-categorical-field"]').should('not.contain', 'All');
       cy.get('[data-cy="Neurobagel graph-categorical-field"]').find('[data-testid="CloseIcon"]').click();
       cy.get('[data-cy="Neurobagel graph-categorical-field"]').should('contain', 'All');
+
       cy.get('[data-cy="Neurobagel graph-categorical-field"]').type('OpenNeuro{downarrow}{enter}');
       cy.get('[data-cy="openneuro-alert"]').should('be.visible');
+
       cy.get('[data-cy="openneuro-alert"]').find('[data-testid="CloseIcon"]').click();
       cy.get('[data-cy="openneuro-alert"]').should('not.exist');
     });
