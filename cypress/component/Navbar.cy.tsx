c312006b49144095a001d8d31dbcda90b74b3b4e
rmanaem <rmanaem@protonmail.ch>
2024-02-26 10:30:21 -0500

    Update `Navbar` component test

--- cypress/component/Navbar.cy.tsx
+++ cypress/component/Navbar.cy.tsx
@@ -4,11 +4,7 @@ describe('Navbar', () => {
   it('Displays a MUI Toolbar with logo, title, subtitle, documentation link, and GitHub link', () => {
     cy.mount(<Navbar />);
     cy.get("[data-cy='navbar']").should('be.visible');
-    cy.get("[data-cy='navbar'] img").should(
-      'have.attr',
-      'src',
-      'https://raw.githubusercontent.com/neurobagel/documentation/main/docs/imgs/logo/neurobagel_logo.png'
-    );
+    cy.get("[data-cy='navbar'] img").should('exist');
     cy.get("[data-cy='navbar'] h5").should('contain', 'Neurobagel Query');
     cy.get("[data-cy='navbar'] p").should(
       'contain',
