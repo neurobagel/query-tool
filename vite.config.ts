c848de76dee586f4527a6901d819f5138a3442b4
rmanaem <rmanaem@protonmail.ch>
2024-02-29 17:30:45 -0500

    Implemented a virtualization solution for rendering ResultCards

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import fixReactVirtualized from 'esbuild-plugin-react-virtualized'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,11 +10,14 @@ export default defineConfig(({ mode }) => {
     throw new Error(`Environment variable NB_API_QUERY_URL is not defined.`);
   }
 
+  // fixReactVirtualized plugin is added here as a fix for a dependency
+  // currently present in react-vritualized
+  // see: https://github.com/bvaughn/react-virtualized/issues/1722#issuecomment-1911672178
   return {
     preview: {
       port: 5173,
     },
-    plugins: [react()],
+    plugins: [react(), fixReactVirtualized,],
     envPrefix: 'NB_',
   };
 });
