da3c4c8d2e697001fab3c72fc7a82b664a4e624d
rmanaem <rmanaem@protonmail.ch>
2024-07-06 16:35:40 -0400

    Wrapped the app with `GoogleOAuthProvider`

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import './index.css';
 
@@ -13,10 +14,12 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* CSS injection order for MUI and tailwind: https://mui.com/material-ui/guides/interoperability/#tailwind-css */}
-    <StyledEngineProvider injectFirst>
-      <RouterProvider router={router} />
-    </StyledEngineProvider>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="465352721782-aj7eam9jdu967adj8vd8ckih325k62d5.apps.googleusercontent.com">
+    <React.StrictMode>
+      {/* CSS injection order for MUI and tailwind: https://mui.com/material-ui/guides/interoperability/#tailwind-css */}
+      <StyledEngineProvider injectFirst>
+        <RouterProvider router={router} />
+      </StyledEngineProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
