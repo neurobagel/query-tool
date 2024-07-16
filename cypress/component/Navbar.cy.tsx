0cd91a99397819b910e3bfbbeff8623c89891ea8
rmanaem <rmanaem@protonmail.ch>
2024-07-16 13:46:58 -0400

    Updated `Navbar` component test

--- cypress/component/Navbar.cy.tsx
+++ cypress/component/Navbar.cy.tsx
@@ -1,14 +1,24 @@
 import Navbar from '../../src/components/Navbar';
 
 const props = {
+  isLoggedIn: true,
   name: 'john doe',
   profilePic: 'johndoe.png',
   onLogout: () => {},
+  onLogin: () => {},
 };
 
 describe('Navbar', () => {
   it('Displays a MUI Toolbar with logo, title, subtitle, documentation link, and GitHub link', () => {
-    cy.mount(<Navbar name={props.name} profilePic={props.profilePic} onLogout={props.onLogout} />);
+    cy.mount(
+      <Navbar
+        isLoggedIn={props.isLoggedIn}
+        name={props.name}
+        profilePic={props.profilePic}
+        onLogout={props.onLogout}
+        onLogin={props.onLogin}
+      />
+    );
     cy.get("[data-cy='navbar']").should('be.visible');
     cy.get("[data-cy='navbar'] img").should('exist');
     cy.get("[data-cy='navbar'] h5").should('contain', 'Neurobagel Query');
@@ -16,8 +26,9 @@ describe('Navbar', () => {
       'contain',
       'Define and find cohorts at the subject level'
     );
+    cy.get("[data-cy='navbar'] a").eq(0).find('svg').should('be.visible');
     cy.get("[data-cy='navbar'] a")
-      .should('contain', 'Documentation')
+      .eq(0)
       .should('have.attr', 'href', 'https://neurobagel.org/query_tool/');
     cy.get("[data-cy='navbar'] a").eq(1).find('svg').should('be.visible');
     cy.get("[data-cy='navbar'] a")
