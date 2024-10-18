ed07ca3875e1f3901e0fb6467ff43d704eca5940
rmanaem <rmanaem@protonmail.ch>
2024-10-18 11:46:28 -0400

    Implemented a mui theme

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const NBTheme = createTheme({
+  palette: {
+    primary: {
+      light: '#ed937b',
+      main: '#e17e93',
+      dark: '#df799a',
+      contrastText: '#fff',
+    },
+  },
+});
+
+export default NBTheme;
