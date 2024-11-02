c8353790bb121cfc2b1672ce121f02d64b8cb384
rmanaem <rmanaem@protonmail.ch>
2024-11-02 14:33:10 -0400

    Removed unncessary styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      gridTemplateRows: {
-        9: 'repeat(9, auto)',
-        10: 'repeat(10, auto)',
-      },
-    },
-  },
+  theme: {},
   plugins: [],
   corePlugins: {
     preflight: false,
