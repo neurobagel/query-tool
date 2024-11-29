975720505e15383daa970da0e85eb013490d7835
Sebastian Urchs <sebastian.urchs@gmail.com>
2024-11-28 20:31:12 -0500

    Make sure to close auth dialog first

--- cypress/e2e/Feedback.cy.ts
+++ cypress/e2e/Feedback.cy.ts
@@ -74,6 +74,11 @@ describe('Feedback', () => {
   it('Displays and closes small screen size dialog', () => {
     cy.viewport(766, 500);
     cy.visit('/');
+    // TODO: remove this
+    // Bit of a hacky way to close the auth dialog
+    // But we need to do it until we make auth an always-on feature
+    // Because the auth dialog will overlap a lot of the UI and thus fail the tests
+    cy.get('[data-cy="close-auth-dialog-button"]').click();
     cy.get('[data-cy="small-screen-size-dialog"]').should('be.visible');
     cy.get('[data-cy="close-small-screen-size-dialog-button"]').click();
     cy.get('[data-cy="small-screen-size-dialog"]').should('not.exist');
