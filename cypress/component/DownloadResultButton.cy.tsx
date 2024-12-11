7f2dba0e11dc2fd0cfe83bd9086a9cc5275e3ad4
rmanaem <rmanaem@protonmail.ch>
2024-12-11 05:38:57 -0500

    Implemented splitbutton for `DownloadResultButton`

--- cypress/component/DownloadResultButton.cy.tsx
+++ cypress/component/DownloadResultButton.cy.tsx
@@ -1,45 +1,43 @@
 import DownloadResultButton from '../../src/components/DownloadResultButton';
 
 const props = {
-  identifier: 'test',
   disabled: false,
   handleClick: () => {},
 };
 
 describe('DownloadResultButton', () => {
   it('Displays an enabled MUI Button with the identifier passed as prop', () => {
-    cy.mount(
-      <DownloadResultButton
-        identifier={props.identifier}
-        disabled={props.disabled}
-        handleClick={props.handleClick}
-      />
+    cy.mount(<DownloadResultButton disabled={props.disabled} handleClick={props.handleClick} />);
+    cy.get('[data-cy="download-results-button"]').should('be.visible');
+    cy.get('[data-cy="download-results-button"]').should(
+      'contain',
+      'Download selected query results'
     );
-    cy.get('[data-cy="test-download-results-button"]').should('be.visible');
-    cy.get('[data-cy="test-download-results-button"]').should('contain', 'Download test result');
-    cy.get('[data-cy="test-download-results-button"]').should('not.be', 'disabled');
+    cy.get('[data-cy="download-results-button"]').should('not.be', 'disabled');
+  });
+  it.only('Switches between the two button states of the download result button', () => {
+    cy.mount(<DownloadResultButton disabled={props.disabled} handleClick={props.handleClick} />);
+    cy.get('[data-cy="download-results-dropdown-button"]').click();
+    cy.contains('URIs').click();
+    cy.get('[data-cy="download-results-button"]').should(
+      'contain',
+      'Download selected query results with URIs'
+    );
+    cy.get('[data-cy="download-results-dropdown-button"]').click();
+    cy.contains(/results$/).click();
+    cy.get('[data-cy="download-results-button"]')
+      .invoke('text')
+      .should('match', /^Download selected query results$/);
   });
   it('Displays a disabled MUI Button and a tooltip when the button is hovered over', () => {
-    cy.mount(
-      <DownloadResultButton
-        identifier={props.identifier}
-        disabled
-        handleClick={props.handleClick}
-      />
-    );
-    cy.get('[data-cy="test-download-results-button"]').trigger('mouseover', { force: true });
+    cy.mount(<DownloadResultButton disabled handleClick={props.handleClick} />);
+    cy.get('[data-cy="download-results-button"]').trigger('mouseover', { force: true });
     cy.get('.MuiTooltip-tooltip').should('contain', 'Please select at least one dataset');
   });
   it('Fires the handleClick event handler with the appropriate payload when the button is clicked', () => {
     const handleClickSpy = cy.spy().as('handleClickSpy');
-    cy.mount(
-      <DownloadResultButton
-        identifier={props.identifier}
-        disabled={props.disabled}
-        handleClick={handleClickSpy}
-      />
-    );
-    cy.get('[data-cy="test-download-results-button"]').click();
-    cy.get('@handleClickSpy').should('have.been.calledWith', 'test');
+    cy.mount(<DownloadResultButton disabled={props.disabled} handleClick={handleClickSpy} />);
+    cy.get('[data-cy="download-results-button"]').click();
+    cy.get('@handleClickSpy').should('have.been.calledWith', 0);
   });
 });
