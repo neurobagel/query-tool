e0020f1567008fbc4f40ca0b577ba5638cb5a122
rmanaem <rmanaem@protonmail.ch>
2025-01-10 15:50:12 -0500

    Set the specPattern for cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/*',
     experimentalStudio: true,
     baseUrl: 'http://localhost:5173',
   },
 
   component: {
+    specPattern: 'cypress/component/*',
     devServer: {
       framework: 'react',
       bundler: 'vite',
