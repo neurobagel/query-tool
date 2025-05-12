459aac19daa08c769c59b460eab2fa3ab149841d
rmanaem <rmanaem@protonmail.ch>
2025-05-12 18:22:27 -0400

    Revert "Let's just mock the responses"
    This reverts commit eb5e4f6208fd338286e914a25324516d6faedca3.

--- cypress/e2e/UpdateExamples.cy.ts
+++ cypress/e2e/UpdateExamples.cy.ts
@@ -1,23 +1,16 @@
 // This file automates the process of generating the query tool example files.
 import fapiQuerySuccess200 from '../../neurobagel_examples/api-responses/fapi_query_success_200.json';
-import { diagnosisOptions, assessmentToolOptions } from '../fixtures/mocked-responses';
 
 describe('Update Examples', () => {
   it('Generates result files using a successful FAPI query', () => {
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/diagnoses',
-      },
-      diagnosisOptions
-    ).as('getDiagnosisOptions');
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/assessments',
-      },
-      assessmentToolOptions
-    ).as('getAssessmentToolOptions');
+    cy.intercept({
+      method: 'GET',
+      url: '/diagnoses',
+    }).as('getDiagnosisOptions');
+    cy.intercept({
+      method: 'GET',
+      url: '/assessments',
+    }).as('getAssessmentToolOptions');
     cy.intercept('GET', 'query*', (req) => {
       req.reply(fapiQuerySuccess200);
     }).as('call');
