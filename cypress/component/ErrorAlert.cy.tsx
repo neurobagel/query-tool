48b8e0227e136cd285f1d066e76ec5ef76ad65b1
Sebastian Urchs <sebastian.urchs@gmail.com>
2025-07-25 10:31:44 -0400

    Basic prop passing

--- cypress/component/ErrorAlert.cy.tsx
+++ cypress/component/ErrorAlert.cy.tsx
@@ -1,7 +1,12 @@
 import ErrorAlert from '../../src/components/ErrorAlert';
 
+const errorTitle = 'errorTitle';
+const errorMessage = 'errorMessage';
+
 describe('ErrorAlert', () => {
   it('Render', () => {
-    cy.mount(<ErrorAlert />);
+    cy.mount(<ErrorAlert errorTitle={errorTitle} errorMessage={errorMessage} />);
+    cy.get('[data-cy="error-alert"]').contains(errorTitle);
+    cy.get('[data-cy="error-alert"]').contains(errorMessage);
   });
 });
--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -1,5 +1,15 @@
-function ErrorAlert() {
-  return <div>Hi</div>;
+interface ErrorAlertProps {
+  errorTitle: string;
+  errorMessage: string;
+}
+
+function ErrorAlert({ errorTitle, errorMessage }: ErrorAlertProps) {
+  return (
+    <div data-cy="error-alert">
+      <h1>{errorTitle}</h1>
+      <p>{errorMessage}</p>
+    </div>
+  );
 }
 
 export default ErrorAlert;
