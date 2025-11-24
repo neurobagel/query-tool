bfb12a5cf886ab0ac2cfda274fdaaa3d355d3eec
Sebastian Urchs <sebastian.urchs@gmail.com>
2025-11-24 15:13:06 -0500

    [FIX] fix issue in component tests
    They were lagging behind the implementation

--- cypress/component/NodeAdmonition.cy.tsx
+++ cypress/component/NodeAdmonition.cy.tsx
@@ -1,23 +1,55 @@
 import NodeAdmonition from '../../src/components/NodeAdmonition';
 
 describe('<NodeAdmonition />', () => {
-  it('should render admonitions for provided nodes', () => {
+  it('should render single node as simple alert', () => {
+    cy.mount(<NodeAdmonition nodes={['OpenNeuro']} onDismiss={cy.stub()} />);
+
+    cy.get('[data-cy="openneuro-alert"]').should('be.visible');
+    cy.contains('The OpenNeuro node is being actively annotated').should('be.visible');
+    cy.get('[role="tab"]').should('not.exist');
+  });
+
+  it('should render multiple nodes with horizontal tabs', () => {
     cy.mount(<NodeAdmonition nodes={['OpenNeuro', 'EBRAINS']} onDismiss={cy.stub()} />);
 
-    cy.get('[data-cy="openneuro-alert"]').should('exist');
+    cy.get('[role="tab"]').should('have.length', 2);
+    cy.contains('[role="tab"]', 'OpenNeuro').should('be.visible');
+    cy.contains('[role="tab"]', 'EBRAINS').should('be.visible');
+
+    // First tab should be selected by default
+    cy.get('[data-cy="openneuro-alert"]').should('be.visible');
+    cy.contains('The OpenNeuro node is being actively annotated').should('be.visible');
+  });
+
+  it('should switch content when clicking different tabs', () => {
+    cy.mount(<NodeAdmonition nodes={['OpenNeuro', 'EBRAINS']} onDismiss={cy.stub()} />);
+
+    // First tab should be selected by default
+    cy.get('[data-cy="openneuro-alert"]').should('be.visible');
     cy.contains('The OpenNeuro node is being actively annotated').should('be.visible');
 
-    cy.get('[data-cy="ebrains-alert"]').should('exist');
+    cy.contains('[role="tab"]', 'EBRAINS').click();
+    cy.get('[data-cy="ebrains-alert"]').should('be.visible');
     cy.contains('The EBRAINS node is being actively annotated').should('be.visible');
   });
 
-  it('should emit dismiss event with node name when close button is clicked', () => {
+  it('should emit dismiss event with node name when close button in tab is clicked', () => {
+    const onDismissSpy = cy.stub().as('dismissHandler');
+
+    cy.mount(<NodeAdmonition nodes={['OpenNeuro', 'EBRAINS']} onDismiss={onDismissSpy} />);
+
+    cy.contains('[role="tab"]', 'OpenNeuro').find('button').click();
+    cy.get('@dismissHandler').should('have.been.calledOnceWith', 'OpenNeuro');
+  });
+
+  it('should emit dismiss event for single node alert', () => {
     const onDismissSpy = cy.stub().as('dismissHandler');
 
     cy.mount(<NodeAdmonition nodes={['OpenNeuro']} onDismiss={onDismissSpy} />);
 
-    cy.get('[data-cy="openneuro-alert"]').should('be.visible');
-    cy.get('[data-cy="openneuro-alert"] button').click();
+    cy.get('[data-cy="openneuro-alert"]').within(() => {
+      cy.get('button').click();
+    });
     cy.get('@dismissHandler').should('have.been.calledOnceWith', 'OpenNeuro');
   });
 
@@ -31,21 +63,5 @@ describe('<NodeAdmonition />', () => {
     cy.mount(<NodeAdmonition nodes={[]} onDismiss={cy.stub()} />);
 
     cy.get('[role="alert"]').should('not.exist');
-  });
-
-  it('should render multiple admonitions stacked vertically', () => {
-    cy.mount(<NodeAdmonition nodes={['OpenNeuro', 'EBRAINS']} onDismiss={cy.stub()} />);
-
-    cy.get('[data-cy="openneuro-alert"]').should('exist');
-    cy.get('[data-cy="ebrains-alert"]').should('exist');
-
-    // Check they're stacked (ebrains should be below openneuro)
-    cy.get('[data-cy="openneuro-alert"]').then(($openneuro) => {
-      cy.get('[data-cy="ebrains-alert"]').then(($ebrains) => {
-        expect($ebrains[0].getBoundingClientRect().top).to.be.greaterThan(
-          $openneuro[0].getBoundingClientRect().bottom
-        );
-      });
-    });
   });
 });
