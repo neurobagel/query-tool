96c8b4dd40a8c91d10a824d656fcd52ba49b7b0a
Tusharjamdade <tusharnjamdade@gmail.com>
2025-12-03 01:05:29 +0530

    test fix

--- cypress/component/DownloadResultButton.cy.tsx
+++ cypress/component/DownloadResultButton.cy.tsx
@@ -7,7 +7,7 @@ const props = {
 };
 
 describe('DownloadResultButton', () => {
-  it('Displays an enabled MUI Button with the identifier passed as prop', () => {
+  it('Displays an enabled MUI Download button and radio options', () => {
     cy.mount(
       <DownloadResultButton
         disabled={props.disabled}
@@ -16,13 +16,13 @@ describe('DownloadResultButton', () => {
       />
     );
     cy.get('[data-cy="download-results-button"]').should('be.visible');
-    cy.get('[data-cy="download-results-button"]').should(
-      'contain',
-      'Download selected query results'
-    );
-    cy.get('[data-cy="download-results-button"]').should('not.be', 'disabled');
+    cy.get('[data-cy="download-results-button"]').should('not.be.disabled');
+    cy.get('[data-cy="download-results-button"]').should('contain', 'Download');
+    cy.get('[data-cy="download-radio-0"]').should('contain', 'Include term labels');
+    cy.get('[data-cy="download-radio-1"]').should('contain', 'Include term URIs');
   });
-  it('Fires the handleClick event handler with the appropriate payload when the button is clicked', () => {
+
+  it('Calls handleClick(1) when "Include term URIs" is selected and Download clicked', () => {
     const handleClickSpy = cy.spy().as('handleClickSpy');
     cy.mount(
       <DownloadResultButton
@@ -31,28 +31,13 @@ describe('DownloadResultButton', () => {
         loading={props.loading}
       />
     );
-    cy.get('[data-cy="download-results-dropdown-button"]').click();
-    cy.contains('URIs').click();
-    cy.get('[data-cy="download-results-button"]').should(
-      'contain',
-      'Download selected query results with URIs'
-    );
+
+    cy.get('[data-cy="download-radio-1"]').click();
     cy.get('[data-cy="download-results-button"]').click();
     cy.get('@handleClickSpy').should('have.been.calledWith', 1);
-    cy.get('[data-cy="download-results-dropdown-button"]').click();
-    cy.contains(/results$/).click();
-    cy.get('[data-cy="download-results-button"]')
-      .invoke('text')
-      .should('match', /^Download selected query results$/);
-    cy.get('[data-cy="download-results-button"]').click();
-    cy.get('@handleClickSpy').should('have.been.calledWith', 0);
   });
-  it('Displays a disabled MUI Button and a tooltip when the button is hovered over', () => {
-    cy.mount(<DownloadResultButton disabled handleClick={props.handleClick} loading={false} />);
-    cy.get('[data-cy="download-results-button"]').trigger('mouseover', { force: true });
-    cy.get('.MuiTooltip-tooltip').should('contain', 'Please select at least one dataset');
-  });
-  it('Fires the handleClick event handler with the appropriate payload when the button is clicked', () => {
+
+  it('Calls handleClick(0) when default (labels) option is used', () => {
     const handleClickSpy = cy.spy().as('handleClickSpy');
     cy.mount(
       <DownloadResultButton
@@ -64,10 +49,16 @@ describe('DownloadResultButton', () => {
     cy.get('[data-cy="download-results-button"]').click();
     cy.get('@handleClickSpy').should('have.been.calledWith', 0);
   });
-  it('Disables the button and displays a loading spinner when loading is true', () => {
-    cy.mount(
-      <DownloadResultButton disabled={props.disabled} handleClick={props.handleClick} loading />
-    );
+
+  it('Displays tooltip when disabled', () => {
+    cy.mount(<DownloadResultButton disabled handleClick={props.handleClick} loading={false} />);
+
+    cy.get('[data-cy="download-results-button"]').parent().trigger('mouseover', { force: true });
+    cy.get('.MuiTooltip-tooltip').should('contain', 'Please select at least one dataset');
+  });
+
+  it('Shows spinner and disables button when loading is true', () => {
+    cy.mount(<DownloadResultButton disabled={false} handleClick={props.handleClick} loading />);
     cy.get('[data-cy="download-results-button"]').should('be.disabled');
     cy.get('[data-cy="download-results-button"]').should(
       'contain',
